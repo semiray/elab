671cf04ea991a3529ea60d213d174164c8e59dd3
Semiray Girgis <semiray.girgis@cern.ch>
2016-05-02 00:03:23 +0300

    elab Session 1

--- lab/ex_1.6_Display/ex_1.6_Display.ino
+++ lab/ex_1.6_Display/ex_1.6_Display.ino
@@ -12,20 +12,7 @@ The circuit:
  * LCD VCC pin to 5V
  * 10K resistor:
  * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ * wiper to LCD VO pin 
  */
 
 // include the library code:
@@ -33,7 +20,7 @@ The circuit:
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12,11,5,4,3,2);
-//LiquidCrystal lcd(12,11,8,3,2,1);
+
 
  int pin11 = 0;
 
@@ -47,7 +34,6 @@ void setup() {
 void loop() {
   
   // set the cursor to column 0, line 1
-  // (note: line 1 is the second row since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
