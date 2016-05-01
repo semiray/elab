962f8b5625a96c6fe4e1423da7fb99662617db93
Semiray Girgis <semiray.girgis@cern.ch>
2016-05-02 00:42:13 +0300

    elab Session 1

--- lab/ex_1.8_Stopwatch/ex_1.8_Stopwatch.ino
+++ lab/ex_1.8_Stopwatch/ex_1.8_Stopwatch.ino
@@ -1,38 +1,37 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //include library
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-//LiquidCrystal lcd( 12,11,10,9,8,7);
 
  
-int ledPin = 13;                    // LED connected to digital pin 13
-int buttonPin = 2;                  // button on pin 2
+int Pin_Led = 13;                    // LED connected to digital pin 13
+int Pin_Button = 2;                  // button on pin 2
  
  
 int value = LOW;                    // previous value of the LED
-int buttonState;                    // variable to store button state
-int lastButtonState;                // variable to store last button state
-int blinking;                       // condition for blinking - timer is timing
-int frameRate = 100;                // the frame rate (frames per second) at which the stopwatch runs - Change to suit
-long interval = (1000/frameRate);   // blink interval
+int buttonState;                   
+int lastButtonState;                
+int blinking;                       
+int frameRate = 100;                // the frame rate at which the stopwatch runs 
+long timeslot = (1000/frameRate);   // blink interval
 long previousMillis = 0;            // variable to store last time LED was updated
-long startTime ;                    // start time for stop watch
-long elapsedTime ;                  // elapsed time for stop watch
+long startTime ;                    // start time for stopwatch
+long passedTime ;                  // passed time for stopwatch
 int fractional;                     // variable used to store fractional part of Frames
-int fractionalSecs;                 // variable used to store fractional part of Seconds
-int fractionalMins;                 // variable used to store fractional part of Minutes
-int elapsedFrames;                  // elapsed frames for stop watch
-int elapsedSeconds;                 // elapsed seconds for stop watch
-int elapsedMinutes;                 // elapsed Minutes for stop watch
-char buf[10];                       // string buffer for itoa function
+int fractionalSecs;                
+int fractionalMins;                 
+int passedFrames;                  // passed frames for stopwatch
+int passedSeconds;                 
+int passedMinutes;                 
+char buf[10];                       
  
  
 void setup()
 {
-  lcd.begin(16, 2);                // intialise the LCD.
-  pinMode(ledPin, OUTPUT);         // sets the digital pin as output
-  pinMode(buttonPin, INPUT);       // not really necessary, pins default to INPUT anyway
-  digitalWrite(buttonPin, HIGH);   // turn on pullup resistors. Wire button so that press shorts pin to ground.
-lcd.setCursor(0,0);
+  lcd.begin(16, 2);                
+  pinMode(Pin_Led, OUTPUT);         
+  pinMode(Pin_Button, INPUT);      
+  digitalWrite(Pin_Button, HIGH);   
+  lcd.setCursor(0,0);
   lcd.print("Digital Clock is");
   lcd.setCursor(5,1);
   lcd.print("READY");
@@ -42,125 +41,115 @@ lcd.setCursor(0,0);
 void loop(){
  
  
-  digitalWrite(ledPin, LOW);            // Initiate LED and Step Pin States
+  digitalWrite(Pin_Led, LOW);            
  
  
-  buttonState = digitalRead(buttonPin); // Check for button press, read the button state and store
+  buttonState = digitalRead(Pin_Button); 
  
- 
-// check for a high to low transition if true then found a new button press while clock is not running - start the clock   
+  
    if (buttonState == LOW && lastButtonState == HIGH  &&  blinking == false){
-    startTime = millis();                               // store the start time
-      blinking = true;                                  // turn on blinking while timing
-      delay(10);                                         // short delay to debounce switch
+      startTime = millis();                              
+      blinking = true;                                  
+      delay(10);                                        
       lastButtonState = buttonState;                    // store buttonState in lastButtonState, to compare next time
    }
  
  
-// check for a high to low transition if true then found a new button press while clock is running - stop the clock and report
    else if (buttonState == LOW && lastButtonState == HIGH && blinking == true){
-   blinking = false;                                    // turn off blinking, all done timing
-   lastButtonState = buttonState;                       // store buttonState in lastButtonState, to compare next time
+   blinking = false;                                   
+   lastButtonState = buttonState;                    
+ 
+         
+   passedTime =   millis() - startTime;                // store passed time
+   passedMinutes = (passedTime / 60000L);
+   passedSeconds = (passedTime / 1000L);              // divide by 1000 to convert to seconds
+   passedFrames = (passedTime / timeslot);            // divide by 100 to convert to 1/100 of a second
+   fractional = (int)(passedFrames % frameRate);      
+   fractionalSecs = (int)(passedSeconds % 60L);        
+   fractionalMins = (int)(passedMinutes % 60L);        
+   lcd.clear();                                        
  
  
-// Routine to report elapsed time           
-   elapsedTime =   millis() - startTime;                // store elapsed time
-   elapsedMinutes = (elapsedTime / 60000L);
-   elapsedSeconds = (elapsedTime / 1000L);              // divide by 1000 to convert to seconds - then cast to an int to print
-   elapsedFrames = (elapsedTime / interval);            // divide by 100 to convert to 1/100 of a second - then cast to an int to print
-   fractional = (int)(elapsedFrames % frameRate);       // use modulo operator to get fractional part of 100 Seconds
-   fractionalSecs = (int)(elapsedSeconds % 60L);        // use modulo operator to get fractional part of 60 Seconds
-   fractionalMins = (int)(elapsedMinutes % 60L);        // use modulo operator to get fractional part of 60 Minutes
-   lcd.clear();                                         // clear the LDC
- 
- 
-if (fractionalMins < 10){                            // pad in leading zeros
-      lcd.print("0");                                 // add a zero
+if (fractionalMins < 10){                           
+      lcd.print("0");                               
       }
  
  
-    lcd.print(itoa(fractionalMins, buf, 10));       // convert the int to a string and print a fractional part of 60 Minutes to the LCD
-      lcd.print(":");                                 //print a colan.
+    lcd.print(itoa(fractionalMins, buf, 10));      
+      lcd.print(":");                                 
  
  
-if (fractionalSecs < 10){                            // pad in leading zeros
-      lcd.print("0");                                 // add a zero
+if (fractionalSecs < 10){                            
+      lcd.print("0");                                 
       }
  
  
-lcd.print(itoa(fractionalSecs, buf, 10));          // convert the int to a string and print a fractional part of 60 Seconds to the LCD
-   lcd.print(":");                                    //print a colan.
+lcd.print(itoa(fractionalSecs, buf, 10));         
+   lcd.print(":");                                    
  
  
-if (fractional < 10){                                // pad in leading zeros
-      lcd.print("0");                                 // add a zero
+if (fractional < 10){                               
+      lcd.print("0");                                
       }    
  
  
-lcd.print(itoa(fractional, buf, 10));              // convert the int to a string and print a fractional part of 25 Frames to the LCD
+lcd.print(itoa(fractional, buf, 10));              
    }
  
  
 else{
-      lastButtonState = buttonState;                  // store buttonState in lastButtonState, to compare next time
+      lastButtonState = buttonState;                 
    }
  
+
  
-// run commands at the specified time interval
-// blink routine - blink the LED while timing
-// check to see if it's time to blink the LED; that is, the difference
-// between the current time and last time we blinked the LED is larger than
-// the interval at which we want to blink the LED.
- 
- 
-if ( (millis() - previousMillis > interval) ) {
+if ( (millis() - previousMillis > timeslot) ) {
  
  
     if (blinking == true){
-       previousMillis = millis();                    // remember the last time we blinked the LED
+       previousMillis = millis();                
  
  
-       digitalWrite(ledPin, HIGH);                   // Pulse the LED for Visual Feedback
+       digitalWrite(Pin_Led, HIGH);                   
  
  
-       elapsedTime =   millis() - startTime;         // store elapsed time
-         elapsedMinutes = (elapsedTime / 60000L);      // divide by 60000 to convert to minutes - then cast to an int to print
-         elapsedSeconds = (elapsedTime / 1000L);       // divide by 1000 to convert to seconds - then cast to an int to print
-         elapsedFrames = (elapsedTime / interval);     // divide by 40 to convert to 1/25 of a second - then cast to an int to print
-         fractional = (int)(elapsedFrames % frameRate);// use modulo operator to get fractional part of 25 Frames
-         fractionalSecs = (int)(elapsedSeconds % 60L); // use modulo operator to get fractional part of 60 Seconds
-         fractionalMins = (int)(elapsedMinutes % 60L); // use modulo operator to get fractional part of 60 Minutes
-         lcd.clear();                                  // clear the LDC
+         passedTime =   millis() - startTime;         
+         passedMinutes = (passedTime / 60000L);      
+         passedSeconds = (passedTime / 1000L);       
+         passedFrames = (passedTime / timeslot);     
+         fractional = (int)(passedFrames % frameRate);
+         fractionalSecs = (int)(passedSeconds % 60L); 
+         fractionalMins = (int)(passedMinutes % 60L); 
+         lcd.clear();                                  
  
  
-       if (fractionalMins < 10){                     // pad in leading zeros
-         lcd.print("0");                             // add a zero
+       if (fractionalMins < 10){                     
+         lcd.print("0");                            
          }
  
  
-       lcd.print(itoa(fractionalMins, buf, 10));   // convert the int to a string and print a fractional part of 60 Minutes to the LCD
-         lcd.print(":");                             //print a colan.
+       lcd.print(itoa(fractionalMins, buf, 10));   
+         lcd.print(":");                             
  
  
-       if (fractionalSecs < 10){                     // pad in leading zeros
-         lcd.print("0");                             // add a zero
+       if (fractionalSecs < 10){                     
+         lcd.print("0");                            
          }
  
  
-       lcd.print(itoa(fractionalSecs, buf, 10));   // convert the int to a string and print a fractional part of 60 Seconds to the LCD
-         lcd.print(":");                             //print a colan.
+       lcd.print(itoa(fractionalSecs, buf, 10));   
+         lcd.print(":");                             
  
  
-       if (fractional < 10){                         // pad in leading zeros
-         lcd.print("0");                             // add a zero
+       if (fractional < 10){                         
+         lcd.print("0");                             
          }
-          lcd.print(itoa((fractional), buf, 10));  // convert the int to a string and print a fractional part of 25 Frames to the LCD
+          lcd.print(itoa((fractional), buf, 10));  
          }
  
  
     else{
-          digitalWrite(ledPin, LOW);                 // turn off LED when not blinking
-
+          digitalWrite(Pin_Led, LOW);                 
           
           }
 }
